157d353d27f3120207f567cd8b313e8718792e50
Borna Maticic <bornamaticic@yahoo.com>
2019-01-03 23:11:27 +0000

    fixed gem issues.

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require('rubygems')
 require_relative('controllers/sessions_controller')
 require_relative('controllers/members_controller')
 require_relative('controllers/bookings_controller')
 require_relative('controllers/instructors_controller')
-require 'bundler/capistrano'
+
 
 get '/' do
   erb( :index )
