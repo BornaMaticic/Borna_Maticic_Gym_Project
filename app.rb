4e491f5a763b14b9e4b02659990045e051fdab94
Borna Maticic <bornamaticic@yahoo.com>
2019-01-03 22:20:24 +0000

    added gems and buildpack.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative('controllers/sessions_controller')
 require_relative('controllers/members_controller')
 require_relative('controllers/bookings_controller')
 require_relative('controllers/instructors_controller')
+require 'bundler/capistrano'
 
 get '/' do
   erb( :index )
